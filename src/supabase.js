2132804aa8fb25b32bafcb35bc4b9b68eb77db1a
The Wizard of Hahz <thewizardofhahz@king-hahz.attlocal.net>
2023-03-07 12:19:47 -0500

    ai

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import {createClient} from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jixkwpgwbrzsliyprmje.supabase.co/rest/v1/ai?select=source';
+const supabaseUrl = 'https://jixkwpgwbrzsliyprmje.supabase.co/rest/v1/ai';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeGt3cGd3YnJ6c2xpeXBybWplIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxOTYyNDQsImV4cCI6MTk5Mzc3MjI0NH0.laRIX2zvrfqH-_-t1RzGc230fATv_b4ihu9r1hxzQ9Q';
 // eslint-disable-next-line no-unused-vars
