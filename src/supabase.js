093d104919e9ef6da4036d0b58919fd83e1336cf
The Wizard of Hahz <thewizardofhahz@king-hahz.attlocal.net>
2023-03-07 15:48:03 -0500

    env

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import {createClient} from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://jixkwpgwbrzsliyprmje.supabase.co/rest/v1/ai';
-const supabaseKey =
+const supabaseKey = process.env.SUPABASE_KEY
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeGt3cGd3YnJ6c2xpeXBybWplIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxOTYyNDQsImV4cCI6MTk5Mzc3MjI0NH0.laRIX2zvrfqH-_-t1RzGc230fATv_b4ihu9r1hxzQ9Q';
 // eslint-disable-next-line no-unused-vars
 const supabase = createClient(supabaseUrl, supabaseKey);
