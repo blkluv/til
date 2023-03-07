37fcbf93d51d8489335a4fd400419ee381544353
The Wizard of Hahz <thewizardofhahz@king-hahz.attlocal.net>
2023-03-07 16:33:40 -0500

    supa fly

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,8 @@
-import {createClient} from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jixkwpgwbrzsliyprmje.supabase.co/rest/v1/ai';
-const supabaseKey = process.env.SUPABASE_KEY
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeGt3cGd3YnJ6c2xpeXBybWplIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxOTYyNDQsImV4cCI6MTk5Mzc3MjI0NH0.laRIX2zvrfqH-_-t1RzGc230fATv_b4ihu9r1hxzQ9Q';
-// eslint-disable-next-line no-unused-vars
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_ANON_KEY
 
-export default supabase;
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
